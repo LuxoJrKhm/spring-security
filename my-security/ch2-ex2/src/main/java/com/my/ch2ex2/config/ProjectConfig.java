9c1bfaa59f2ddc1bc6569099089963d1b14a55e4
Hyeongmin <hmkim0966@gmail.com>
2025-12-11 07:11:11 +0900

    feat: my-security 생성, ch2 예제 수행

---
+++ my-security/ch2-ex2/src/main/java/com/my/ch2ex2/config/ProjectConfig.java
@@ -0,0 +1,46 @@
+package com.my.ch2ex2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults()) // 기본 필터들
+                .authorizeHttpRequests((auth) -> auth.
+                        anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailService = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailService.createUser(user);
+        return userDetailService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ my-security/ch2-ex5/src/main/java/com/my/ch2ex5/security/CustomAuthenticationProvider.java
@@ -0,0 +1,38 @@
+package com.my.ch2ex5.security;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+// 이 클래스가 있으면 passwordEncoder랑 userDetailService 빈이 필요가 없다
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if ("john".equals(username) && "12345".equals(password)) {
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    List.of(new SimpleGrantedAuthority("read"))
+            );
+        } else {
+            throw new AuthenticationCredentialsNotFoundException("Error!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
