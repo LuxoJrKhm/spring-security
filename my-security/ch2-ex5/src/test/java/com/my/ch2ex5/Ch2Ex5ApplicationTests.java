3d2967813b0034b6d78eddbf98069c5460ba6c26
Hyeongmin <hmkim0966@gmail.com>
2025-12-11 07:13:48 +0900

    feat: my-security 생성, ch2 예제 수행

---
+++ my-security/ch2-ex5/src/test/java/com/my/ch2ex5/Ch2Ex5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.ch2ex5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch2Ex5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
